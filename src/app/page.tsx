408815a6afdc2cb7a5cf77a1a7f7c27973a82aee
7lele <leoarn100@gmail.com>
2025-03-15 09:11:24 +0100

    1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,70 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <>
+            <header
+            className=" sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur border-grid supports-[backdrop-filter]:bg-slate-900/60">
+            <div className="container-wrapper">
+                <div className="container flex h-14 items-center gap-4 md:gap-4">
+                    <div className="mr-4 hidden md:flex"><a className="mr-4 flex items-center gap-2 lg:mr-6" href="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none" viewBox="0 0 340 340"
+                             className=" ml-4">
+                            <path fill="#D9D9D9"
+                                  d="M340 170A169.997 169.997 0 0 0 235.056 12.94 169.998 169.998 0 0 0 12.941 235.057 169.999 169.999 0 0 0 170 340v-68A101.998 101.998 0 0 1 69.96 150.101 102 102 0 0 1 272 170h68Z"/>
+                            <path fill="#D9D9D9"
+                                  d="M283.34 169.67a113.663 113.663 0 0 1-33.293 80.377 113.662 113.662 0 0 1-80.377 33.293v-11.367a102.307 102.307 0 0 0 94.516-63.153 102.307 102.307 0 0 0 7.787-39.15h11.367Z"/>
+                            <path fill="#D9D9D9"
+                                  d="M340 170a170.002 170.002 0 0 1-170 170v-17a153.003 153.003 0 0 0 141.354-94.449A153.003 153.003 0 0 0 323 170h17Z"/>
+                        </svg>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+                        <span className="hidden font-bold lg:inline-block">Shy UI</span></a>
+                        <nav className="flex items-center gap-4 text-sm xl:gap-6  border-x border-grid h-full my-0 px-4"><a
+                            className="transition-colors hover:text-foreground/80 text-foreground"
+                            href="/docs/installation">Docs</a><a
+                            className="transition-colors hover:text-foreground/80 text-foreground/80"
+                            href="/docs/components">Components</a>
+                        </nav>
+                    </div>
+                    <button
+                        className="inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 hover:text-accent-foreground py-2 h-8 w-full gap-4 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
+                        type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="radix-:R15u6ja:"
+                        data-state="closed">
+                        <a className="mr-4 flex items-center gap-2 " href="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="none"
+                                 viewBox="0 0 340 340" className=" ml-4">
+                                <path fill="#D9D9D9"
+                                      d="M340 170A169.997 169.997 0 0 0 235.056 12.94 169.998 169.998 0 0 0 12.941 235.057 169.999 169.999 0 0 0 170 340v-68A101.998 101.998 0 0 1 69.96 150.101 102 102 0 0 1 272 170h68Z"/>
+                                <path fill="#D9D9D9"
+                                      d="M283.34 169.67a113.663 113.663 0 0 1-33.293 80.377 113.662 113.662 0 0 1-80.377 33.293v-11.367a102.307 102.307 0 0 0 94.516-63.153 102.307 102.307 0 0 0 7.787-39.15h11.367Z"/>
+                                <path fill="#D9D9D9"
+                                      d="M340 170a170.002 170.002 0 0 1-170 170v-17a153.003 153.003 0 0 0 141.354-94.449A153.003 153.003 0 0 0 323 170h17Z"/>
+                            </svg>
+
+
+                            <span className=" font-bold lg:inline-block">Shy UI</span></a>
+                    </button>
+                </div>
+            </div>
+        </header>
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center px-2 py-10 text-center  xl:pe-0 xl:ps-10 xl:text-start ">
+        <div className={"border-t border-b border-grid py-10"}>
+
+
+            <h1 className="font-extrabold text-center text-[clamp(1.5rem,3.5vw,4rem)] leading-[1.1] [word-break:auto-phrase] xl:w-[115%] xl:text-start [:root[dir=rtl]_&amp;]:leading-[1.35]">
+                <div className="font-semibold  [&amp;:first-line]:font-black">Light, Fast,
+                    no install<br /><span className="font-normal">Tailwind CSS Components</span></div>
+            </h1>
+
+            <p className="text-base-content/70 font-title py-4 font-light [text-wrap:balance] md:text-lg xl:text-2xl">Shy UI is a production-ready Tailwind CSS component library <br />which saves time<br />to let
+                    you write less code and focus on what matters.</p>
+
+            <div className="flex w-full items-center justify-center pt-4">
+                <div className="inline-flex flex-col items-stretch justify-center gap-2 px-4 md:flex-row xl:px-0">
+                    <a  href="/docs/install/" className="bg-slate-700 p-3 md:btn-lg group max-w-86 grow rounded-full px-10 hover:bg-slate-600 transition-colors duration-300">Start using Shy UI</a></div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+
+        </>
+    );
 }
